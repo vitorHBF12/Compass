77d3cf20df2b2e2e37fe58e4fc99252daf9e0ce8
Vitor <hugov2716@gmail.com>
2023-10-23 12:10:46 -0400

    Upando arquivos da Sprint7(Certificados, Laboratório, Desafio e Tarefas

--- Sprint7/Desafio/tarefa.py
+++ Sprint7/Desafio/tarefa.py
@@ -3,14 +3,13 @@ import csv
 from datetime import datetime
 
 
-# Configurar informações AWS
 aws_access_key = 'AKIARMW5TT7OLIRAMPZZ'
 aws_secret_key = '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'
 bucket_name = 'desafbucket'
 raw_zone = 'Raw'
 data_source = 'Local'
 data_format = 'CSV'
-data_spec = 'Movies'  # ou 'Series', dependendo do arquivo
+data_spec = 'Movies'
 data_spec2 = 'Series'
 current_date = datetime.now().strftime("%Y/%m/%d")
 file_name = 'movies.csv'
@@ -22,7 +21,6 @@ serie = open(file_name2, "rb")
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key,
                   aws_secret_access_key=aws_secret_key)
 
-# Fazer upload do arquivo para o S3
 s3.upload_file(file_name, bucket_name,
                f"{raw_zone}/{data_source}/{data_format}/{data_spec}/{current_date}/movies.csv")
 s3.upload_file(file_name2, bucket_name,
